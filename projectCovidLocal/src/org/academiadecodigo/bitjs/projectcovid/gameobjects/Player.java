810a81c5326080736c950cf9f05026aa53213ed7
Diogo Paim <diogopaimsteam@gmail.com>
2020-10-16 18:48:31 +0000

    changed nothing significant

--- projectCovidLocal/src/org/academiadecodigo/bitjs/projectcovid/gameobjects/Player.java
+++ projectCovidLocal/src/org/academiadecodigo/bitjs/projectcovid/gameobjects/Player.java
@@ -25,7 +25,7 @@ public class Player {
     public void showAccordingToDirection() {
 
             playerPicture.delete();
-
+   
         switch (fieldPosition.getActualDirection()) {
 
             case LEFT:
